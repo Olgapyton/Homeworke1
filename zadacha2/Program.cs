e3cb3c3580f6ef78b3598f5f95e21ed99206214a
Olga <oborovikova_80@mail.ru>
2023-07-17 22:11:59 +0300

    adeded

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -3,3 +3,20 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: "); // подсказка для пользователя
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max == a");
+}
+else 
+{
+    Console.WriteLine("a < b");
+}
+
+Console.WriteLine();
