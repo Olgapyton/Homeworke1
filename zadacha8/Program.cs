90573e5f2d8261459b64d144d32f46a6fa91d23f
Olga <oborovikova_80@mail.ru>
2023-07-20 20:05:10 +0300

    zadacha8

--- zadacha8/Program.cs
+++ zadacha8/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число: ");
+
+// int num = int.Parse(Console.Redlain());
+
+// int even = 2;
+// while (num >= even)
+// {
+//   Console.Write(even + "");
+//   even = even + 2;
+// }
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i =1;i<n;i++)
+{
+    if(i%2 == 0)
+    System.Console.Write(i+ ",");
+}
